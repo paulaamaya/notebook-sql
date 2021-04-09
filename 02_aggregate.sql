1813e6b5a4a71c8f5a1a261adb3330a6773021a0
Paula Amaya <paula.amaya@mail.utoronto.ca>
2021-04-09 16:01:29 -0400

    Finished aggregate challenge

---
+++ 02_aggregate.sql
@@ -0,0 +1,35 @@
+SELECT COUNT(*)
+FROM actors
+WHERE date_of_birth > '1970-01-01';
+SELECT MAX(domestic_takings)
+FROM movie_revenues;
+SELECT MIN(domestic_takings)
+FROM movie_revenues;
+SELECT SUM(movie_length)
+FROM movies
+WHERE age_certificate = '15';
+SELECT COUNT(*)
+FROM directors
+WHERE nationality = 'Japanese';
+SELECT AVG(movie_length)
+FROM movies
+WHERE movie_lang = 'Chinese';
+--
+SELECT nationality,
+       COUNT(*)
+FROM directors
+GROUP BY nationality
+ORDER BY nationality;
+SELECT movie_lang,
+       age_certificate,
+       SUM(movie_length) AS agg_movie_length
+FROM movies
+GROUP BY movie_lang,
+         age_certificate
+ORDER BY movie_lang;
+SELECT movie_lang,
+       SUM(movie_length) AS agg_movie_length
+FROM movies
+GROUP BY movie_lang
+HAVING SUM(movie_length) > 500
+ORDER BY movie_lang;
